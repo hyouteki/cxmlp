96057a0b66e0ae4991275b4cb692f83af7f3581e
hyouteki <lakshay21060@iiitd.ac.in>
2024-07-27 14:35:23 +0530

    parser works

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 #include "parser.h"
 
 int main() {
-	Steel_LL *tokens = Cxmlp_Tokenize("voyage.xml");
+	Steel_LL *tokens = Cxmlp_Tokenize("./voyage.xml");
     /* Cxmlp_Tokens_Print(tokens); */
 	XmlNode *root = Cxmlp_Parse(tokens);
+	Cxmlp_XmlNode_Print(root);
 	return 0;
 }
